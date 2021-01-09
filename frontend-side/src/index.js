af65201890fba09befdc095d0baa9d9bdc5779e9
Valery Semenenko <gearmobile@gmail.com>
2021-01-09 16:22:45 +0300

    add data in database mongo

--- frontend-side/src/index.js
+++ frontend-side/src/index.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const { connectDB } = require('./helpers/db')
 const { HOST, PORT, MONGODB_URI } = require('./configurations')
-
 const app = express()
 
+const postSchema = new mongoose.Schema({
+  name: String
+})
+const Post = mongoose.model('Post', postSchema)
+
 startServer = () => {
-  app.listen(PORT, () => console.log(`Server started on ${HOST}:${PORT} with database URL ${MONGODB_URI}`))
+  app.listen(PORT, () => {
+    console.log(`Server started on ${HOST}:${PORT} with database URL ${MONGODB_URI}`)
+
+    const post = new Post({ name: 'New Post' })
+    post.save((error, post) => {
+      if (error) return console.log(error)
+      console.log('saved post is: ', post)
+    })
+  })
 }
 
 app.get('/test', (req, res) => {
